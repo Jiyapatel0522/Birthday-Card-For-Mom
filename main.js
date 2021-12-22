d1f8ff3cacc429fcef04362b28cc1a572ab04920
Jiyapatel0522 <87197161+Jiyapatel0522@users.noreply.github.com>
2021-12-22 09:08:20 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+var canvas = new fabric.Canvas('myCanvas');
+ var x= document.getElementById("myAudio");
+ fabric.Image.fromURL(get_image, function(Img{
+
+ })
+
+function new_image()
+{
+	
+	
+}
+
+function playSound(){
+	x.play();
+}
+
+<body class="body_background" onload="new_image()"></body>
+ 
+ block_image_object.set({
+     top:0;
+     left:0;
+ });
+
+ canvas.add(block_image_object);
+ 
